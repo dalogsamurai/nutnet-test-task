8aecf93d4e6615129a1b9f9bde3a63e1840ddc57
dalogsamurai <thelogsamurai@gmail.com>
2023-11-18 15:50:38 +0300

    feat: Header add

---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,15 @@
+import iLogo from "../../assets/logo.svg";
+import "./header.component.sass";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__block">
+        <img src={iLogo} className="header__logo" alt="" />
+      </div>
+      <h1 className="header__title">WeatherCheck</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/icon/icon.component.tsx
@@ -0,0 +1,16 @@
+import Icons from "../../assets/sprite.svg";
+
+interface Props {
+  id: string;
+  className?: string;
+}
+
+const Icon = ({ id, className }: Props) => {
+  return (
+    <svg className={className ? className : ""}>
+      <use href={Icons + `#${id}`} />
+    </svg>
+  );
+};
+
+export default Icon;
