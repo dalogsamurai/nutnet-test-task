c748dc214118c40b90c92c38727158b8a933d9a9
dalogsamurai <thelogsamurai@gmail.com>
2023-11-20 15:58:39 +0300

    feat(Navigation): removeCity add

--- src/components/navigation/navigation.component.tsx
+++ src/components/navigation/navigation.component.tsx
@@ -1,28 +1,46 @@
 import { Link, useParams } from "react-router-dom";
-import iArrowLeft from "../../assets/arrow-left.svg";
-import iBookmark from "../../assets/bookmark.svg";
-import { addNewCity } from "../../utils/localStorageActions";
+import { ReactComponent as IArrowLeft } from "../../assets/arrow-left.svg";
+import { ReactComponent as IBookmark } from "../../assets/bookmark.svg";
+import { ReactComponent as IBookmarkActive } from "../../assets/bookmark-active.svg";
+import {
+	addNewCity,
+	isCityAdded,
+	removeCity,
+} from "../../utils/localStorageActions";
+import { useState } from "react";
 import "./navigation.component.sass";
 
 const Navigation = () => {
 	const { slug } = useParams();
+	const [isBookmarkActive, setBookmarkActive] = useState(isCityAdded(slug!));
 
 	return (
 		<nav className="navigation">
 			<Link to="/">
 				<section className="navigation__button">
-					<img src={iArrowLeft} className="navigation__img" alt="" />
+					<IArrowLeft className="navigation__img" />
 					<p className="navigation__text">Назад</p>
 				</section>
 			</Link>
 			<section className="navigation__button">
-				{/* rome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-				<img
-					src={iBookmark}
-					onClick={() => addNewCity(slug!)}
-					className="navigation__img"
-					alt=""
-				/>
+				{isBookmarkActive && (
+					<IBookmarkActive
+						className="navigation__img"
+						onClick={() => {
+							removeCity(slug!);
+							setBookmarkActive(false);
+						}}
+					/>
+				)}
+				{!isBookmarkActive && (
+					<IBookmark
+						className="navigation__img"
+						onClick={() => {
+							addNewCity(slug!);
+							setBookmarkActive(true);
+						}}
+					/>
+				)}
 			</section>
 		</nav>
 	);
--- src/utils/localStorageActions.ts
+++ src/utils/localStorageActions.ts
@@ -19,3 +19,21 @@ export const addNewCity = (slug: string) => {
 		}
 	}
 };
+
+export const isCityAdded = (slug: string) => {
+	const cities = localStorage.getItem(LS_CITIES);
+	const citiesList = JSON.parse(cities!);
+
+	return citiesList.includes(slug);
+};
+
+export const removeCity = (slug: string) => {
+	const cities = localStorage.getItem(LS_CITIES);
+	const citiesList = JSON.parse(cities!);
+
+	localStorage.setItem(
+		LS_CITIES,
+		// @ts-ignore
+		JSON.stringify([...citiesList.filter((city) => city !== slug)]),
+	);
+};
