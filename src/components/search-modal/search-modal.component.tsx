2347819593f3a37eea212cfbce2600fba2dd46bd
dalogsamurai <thelogsamurai@gmail.com>
2023-11-20 13:26:11 +0300

    fix(SearchModal): Link add

--- src/components/search-modal/search-modal.component.tsx
+++ src/components/search-modal/search-modal.component.tsx
@@ -1,24 +1,24 @@
 import { Link } from "react-router-dom";
-import "./search-modal.component.sass";
 import { ICity } from "../../types/ICity";
+import "./search-modal.component.sass";
 
 interface Props {
-	items: ICity[];
+  items: ICity[];
 }
 
 const SearchModal = ({ items }: Props) => {
-	return (
-		<section className="search-modal">
-			{/* @ts-ignore */}
-			{items.slice(0, 3).map((item) => (
-				<nav className="search-modal__item">
-					<a className="text search-modal__name" href={`/${item.name}`}>
-						{item.name}
-					</a>
-				</nav>
-			))}
-		</section>
-	);
+  return (
+    <section className="search-modal">
+      {/* @ts-ignore */}
+      {items.slice(0, 3).map((item) => (
+        <Link to={`/${item.name}`}>
+          <nav className="search-modal__item">
+            <p className="text search-modal__name">{item.name}</p>
+          </nav>
+        </Link>
+      ))}
+    </section>
+  );
 };
 
 export default SearchModal;
